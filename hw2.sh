b6eaba7eb3be6787d7660174dd0a87c117f6159a
yinhsuan <sandy10235168@gmail.com>
2022-10-18 16:07:09 +0800

    parse json elements

--- hw2.sh
+++ hw2.sh
@@ -1,18 +1,54 @@
 #!/bin/sh
+
+function handler() {
+    declare -a md5Arr
+    declare -a sha1Arr
+    declare -a nameArr
+    declare -a dataArr
+    declare -a hashArr
+
+    # get json elements
+    nameArr=($(cat ${inputFile} | sed -n '{s/ //g; s/"//g; s/,//g; /name/ p;}'))
+    dataArr=($(cat ${inputFile} | sed -n '{s/ //g; s/"//g; s/,//g; /data/ p;}'))
+    hashArr=($(cat ${inputFile} | sed -n '{s/ //g; s/"//g; s/hash:{//g; s/}//g; /md5/ p;}'))
+    hashArr=($(awk -v var="${hashArr[*]}" 'BEGIN {split(var,list,","); for (i=1;i<=length(list);i++) print list[i]}'))
+    for i in ${hashArr[*]}; do
+        md5Arr+=($(echo $i | grep "md5"))
+    done
+    for i in ${hashArr[*]}; do
+        sha1Arr+=($(echo $i | grep "sha-1"))
+    done
+
+
+}
+
+
+inputFile=""
+outputFile=""
+outputDir=""
+isC=0
+isJ=0
+
 OPTERR=0
 while getopts i:o:c:j op; do
     case $op in
         i)
-            echo "i: "${OPTARG}
+            inputFile=${OPTARG}
             ;;
         o)
-            echo "o: "${OPTARG} 
+            outputDir=${OPTARG} 
             ;;
         c)
-            echo "c: "${OPTARG}
+            if [ ${OPTARG} = "csv" ]; then
+                isC=1 #csv
+            else
+                isC=2 #tsv
+            fi
+            outputFile=${OPTARG}
             ;;
         j)
             # Output info.json
+            isJ=1
             ;;
         *)
             {
@@ -27,3 +63,6 @@ while getopts i:o:c:j op; do
             ;;
         esac
 done
+
+# call function => decode the file
+handler
