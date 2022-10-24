00626d8f1b4132e67ba586863491c9458763b846
yinhsuan <sandy10235168@gmail.com>
2022-10-24 19:21:21 +0800

    debug

--- hw2.sh
+++ hw2.sh
@@ -52,6 +52,18 @@ outputDir=""
 isC=0 #isC=1(csv) #isC=2(tsv)
 isJ=0
 
+errorMsg() {
+        echo "hw2.sh -i INPUT -o OUTPUT [-c csv|tsv] [-j]"
+        echo ""
+        echo "Available Options:"
+        echo ""
+        echo "-i: Input file to be decoded"
+        echo "-o: Output directory"
+        echo "-c csv|tsv: Output files.[ct]sv"
+        echo "-j: Output info.json"
+        exit 1
+    } >&2
+
 OPTERR=0
 while getopts i:o:c:j op; do
     case $op in
@@ -75,17 +87,7 @@ while getopts i:o:c:j op; do
             isJ=1
             ;;
         *)
-            {
-                echo "hw2.sh -i INPUT -o OUTPUT [-c csv|tsv] [-j]"
-                echo ""
-                echo "Available Options:"
-                echo ""
-                echo "-i: Input file to be decoded"
-                echo "-o: Output directory"
-                echo "-c csv|tsv: Output files.[ct]sv"
-                echo "-j: Output info.json"
-            } >&2
-            exit 1
+            errorMsg
             ;;
         esac
 done
