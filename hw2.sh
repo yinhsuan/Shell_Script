70b7f278b5feffd0bb0279f5d7b0d165ae3db86d
yinhsuan <sandy10235168@gmail.com>
2022-10-26 17:04:37 +0800

    debug

--- hw2.sh
+++ hw2.sh
@@ -30,27 +30,25 @@ handler() {
     for jsonFile in `ls *json`; do
         dirandname=`cat ${jsonFile} | jq '.name' | sed '{s/"//g;}'`
         data=`cat ${jsonFile} | jq '.data' | sed '{s/"//g;}'`
-        echo "${data}" > "data.txt"
         md5=`cat ${jsonFile} | jq '.hash.md5' | sed '{s/"//g;}'`
         sha1=`cat ${jsonFile} | jq '.hash."sha-1"' | sed '{s/"//g;}'`
-
         dir="$(dirname "${dirandname}")"
         name="$(basename "${dirandname}")"
 
-        # checksum
-        md5Check=`md5sum data.txt | awk 'BEGIN {FS=" "} {print $1}'`
-        sha1Check=`sha1sum data.txt | awk 'BEGIN {FS=" "} {print $1}'`
-        if [ ${md5} != ${md5Check} -o ${sha1} != ${sha1Check} ]; then
-            md5=${md5Check}
-            sha1=${sha1Check}
-            invalidFileCount=$(($invalidFileCount+1))
-        fi
-        
         # create dir & files
         mkdir -p ${outputDir}"/"${dir}
 
         # output files
         echo "${data}" | base64 --decode > ${outputDir}"/"${dirandname}
+
+        # checksum
+        md5Check=`md5sum ${outputDir}"/"${dirandname} | awk 'BEGIN {FS=" "} {print $1}'`
+        sha1Check=`sha1sum ${outputDir}"/"${dirandname} | awk 'BEGIN {FS=" "} {print $1}'`
+        if [ ${md5} != ${md5Check} -o ${sha1} != ${sha1Check} ]; then
+            md5=${md5Check}
+            sha1=${sha1Check}
+            invalidFileCount=$(($invalidFileCount+1))
+        fi
 
         # output to csv or tsv
         size=`ls -l ${outputDir}"/"${dirandname} | awk 'BEGIN {FS=" "} {print $5}'`
